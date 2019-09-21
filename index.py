af5bdc1f8c99a2137aa6172f7cbe05384de438b5
royserafin <rodrigocalser@gmail.com>
2019-09-21 21:31:29 +0100

    Agregar campos desaparecido y usuario

--- index.py
+++ index.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, redirect, url_for
 from flask import Flask
 from flask import request, jsonify
 import base64
@@ -26,15 +26,15 @@ def questions(name=None):
     return render_template('questions.html')
 
 
-@app.route('/caracara/<nombre>/<edad>')
-def cara(nombre, edad):
+@app.route('/caracara/<nombreD>/<edadD>/<estaturaD>/<estadoD>/<municipioD>/<fechaD>/<nombreU>/<edadU>/<estaturaU>/<estadoU>/<municipioU>')
+def cara(nombreD, edadD, estaturaD, estadoD, municipioD, fechaD, nombreU, edadU, estaturaU, estadoU, municipioU):
     # leer el txt o name
     # buscar en el json el nombre y obtener sus datos
     # enviar eso por post a javascript
     
     # leer txt de usuario actual que contiene nombre datos
     # ya se donde esta guardada la foto de la persona,
-    return render_template('caracara.html', nombre = nombre, edad=edad)
+    return render_template('caracara.html', nombreD = nombreD, edadD=edadD, estaturaD = estaturaD, estadoD = estadoD, municipioD = municipioD, fechaD = fechaD, nombreU = nombreU, edadU = edadU, estaturaU = estaturaU, municipioU =municipioU)
 
 
 @app.route('/message.html')
@@ -45,26 +45,31 @@ def message(name=None):
 def camara(name=None):
     return render_template('facedetector/example/camara.html')
 
-@app.route('/image-send', methods=['POST'])
+@app.route('/image-send', methods = ['POST'])
 def imagesend():
 	#i = request.files['foto'].read()
-	
 	data = request.files['imagen'].read()
 	foto_index = request.form["foto"]
 	imgdata = base64.b64decode(data)
 	print('ya')
-
+	
 	filename = 'static/Faces/' + foto_index + '.jpg'  # I assume you have a way of picking unique filenames
-#	with open(filename, 'wb') as f:
-#		f.write(data)
-
-	nombre = fr.encuentra_cara()
+	#	with open(filename, 'wb') as f:
+	#		f.write(data)
+	
+	nombreD = fr.encuentra_cara()
 	text_file=open("match_text.txt",'w')
 	n = text_file.write(nombre)
 	text_file.close()
-	
-	
-	return jsonify(result=nombre)
-    
-	
-
+	edadD= "25"
+	estaturaD = "1.80"
+	estadoD = "Jalisco"
+	municipioD = "Zapopan"
+	fechaD = "15-02-08"
+	nombreU = "Rodolfo Ocampo Blanco"
+	edadU = "25"
+	estaturaU = "1.74"
+	estadoU = "Ciudad de Mexico"
+	municipioU = "Miguel Hidalgo"
+	#return render_template('index.html')
+	return jsonify(nombreD = nombreD, edadD=edadD, estaturaD = estaturaD, estadoD = estadoD, municipioD = municipioD, fechaD = fechaD, nombreU = nombreU, edadU = edadU, estaturaU = estaturaU, estadoU = estadoU, municipioU =municipioU)
